a99389cb4fa06d9e162ff3e4dcff7ec33ec2326f
jordanEscobar <jordanescosoto@gmail.com>
2026-02-03 13:40:44 -0300

    Base de datos - Creación proyecto backend

---
+++ calmatask/src/main/java/com/jordan/calmatask/CalmataskApplication.java
@@ -0,0 +1,13 @@
+package com.jordan.calmatask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalmataskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalmataskApplication.class, args);
+	}
+
+}
